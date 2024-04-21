d1630e9e65519df19c2ace14220800cf53f26359
Refanda Dicky Pradana <refandadicky1@gmail.com>
2024-04-21 12:55:13 +0700

    add feature change name profile

--- app/src/main/java/com/refanda/restoran/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/refanda/restoran/presentation/profile/ProfileViewModel.kt
@@ -2,8 +2,10 @@ package com.refanda.restoran.presentation.profile
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.refanda.restoran.data.model.Profile
 import com.refanda.restoran.data.repository.UserRepository
+import kotlinx.coroutines.Dispatchers
 
 class ProfileViewModel(private val repository: UserRepository) : ViewModel() {
     val profileData = MutableLiveData<Profile>()
@@ -13,9 +15,12 @@ class ProfileViewModel(private val repository: UserRepository) : ViewModel() {
             username = "",
             email = "",
             password = "",
-            profileImg = "https://github.com/refandadicky/kokomputer-assets/blob/main/img_profile/img_profile.jpg?raw=true"
+            profileImg = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fgithub_2111432&psig=AOvVaw350jvPWNYcDD3oIphdsKwA&ust=1713763030849000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDAnffG0oUDFQAAAAAdAAAAABAE"
         )
     }
+    fun updateUsername(newName : String) = repository.updateProfile(
+        fullName = newName
+    ).asLiveData(Dispatchers.IO)
 
     fun getCurrentUser() = repository.getCurrentUser()
 
